eb08cae37a6fcb2f41cf6de9654011b107e0b31f
Sridip Tah <164662749+Sridip-99@users.noreply.github.com>
2025-11-09 21:28:01 +0530

    Update Card.jsx

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ userName, image, about, feedback, YTshotslink }) => {
   return (
     <div className="card">
         <div className="feedback-text-container">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quasi repellat tempore enim iste veritatis ab rem earum libero asperiores eos incidunt nemo fugit at, ducimus officia animi omnis excepturi.</p>
+            <p>{feedback}</p>
         </div>
         <div className="feedback-consumer-container">
             <div className="feedback-consumer-img">
-                <img src="https://static6.depositphotos.com/1003580/615/i/450/depositphotos_6151014-stock-photo-african-american-man-portrait.jpg" alt=""></img>
+                <img src={image} alt={userName + " profile picture"}></img>
             </div>
             <div className="feedback-consumer-info">
-                <h4>Ashok Ghosh</h4>
-                <p>Resident of Madhupur</p>
+                <h4>{userName}</h4>
+                <p>{about}</p>
             </div>
-            <a href="https://youtube.com/shorts/Wf893CqbDa4?si=uqZMJhILZ3meAQar"><i className="fa-brands fa-youtube"></i></a>
+            <a href={YTshotslink}><i className="fa-brands fa-youtube"></i></a>
         </div>
     </div>
   )
